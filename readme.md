b47c0e3e2ae4bc67d51b59a595f9175fded11bdc
Kashito Raiva <tkraiva@gmail.com>
2019-06-03 09:38:54 -0700

    Write removeFromList function

---
+++ readme.md
@@ -0,0 +1,4 @@
+# CodeCore Homework Repo
+
+This is a repo a of all my submission notes
+
--- todo-list/Solution.js
+++ todo-list/Solution.js
@@ -3,3 +3,7 @@ const list=[];
 function addToList(thing,list){
     list.push(thing)
 }
+
+function removeFromList(index,list){
+    list = list.splice(index,1)
+}
